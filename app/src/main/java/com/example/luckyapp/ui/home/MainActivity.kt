c224e62eea81478dd00ef226e690cee2d4dfd459
martin olmos <martino@epidataconsulting.com>
2024-06-29 21:01:08 -0300

    navigation configured

--- app/src/main/java/com/example/luckyapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/luckyapp/ui/home/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.example.luckyapp.ui.home
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.luckyapp.R
 import com.example.luckyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding //nombre de la clase con activity al inicio y binding al final
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //viewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
 
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        //inicio la navegacion con el id del FragmentContainerView y lo casteo a NavHostFragment
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottonNavigation.setupWithNavController(navController)
     }
 
 }
