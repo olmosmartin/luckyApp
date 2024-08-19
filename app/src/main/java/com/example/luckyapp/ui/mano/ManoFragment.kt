75fd89afb58694fc43f38e679b5febdde2d9dac8
martin olmos <martino@epidataconsulting.com>
2024-08-18 21:17:17 -0300

    feat: palmistry screen

--- app/src/main/java/com/example/luckyapp/ui/mano/ManoFragment.kt
+++ app/src/main/java/com/example/luckyapp/ui/mano/ManoFragment.kt
@@ -1,12 +1,17 @@
 package com.example.luckyapp.ui.mano
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.viewModels
 import com.example.luckyapp.databinding.FragmentManoBinding
@@ -29,8 +34,8 @@ class ManoFragment : Fragment() {
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
-            //tiene permisos aceptados
-            //lanzar la camara
+            //si el usuario dio permisos, inicia la cámara
+            startCamera()
         } else {
             Toast.makeText(
                 requireContext(),
@@ -55,7 +60,8 @@ class ManoFragment : Fragment() {
             //si no tiene permisos los pide
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         } else {
-            //si ya tiene los permisos, lanza la camara
+            //si el usuario tiene los permisos ya aceptados, inicia la cámara
+            startCamera()
         }
     }
 
@@ -64,6 +70,33 @@ class ManoFragment : Fragment() {
             requireContext(),
             CAMERA_PERMISSION
         ) == PermissionChecker.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        val cameraProviderFeature = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFeature.addListener({
+            val cameraProvider = cameraProviderFeature.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.PVCamera.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview
+                )
+            } catch (e: Exception) {
+                Log.e("CameraPreview", "Use case binding failed", e)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
 }
